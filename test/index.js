5cb449399badd8652d7ab8366bbdae61176b4365
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2016-06-18 04:41:06 +0400

    Proper tests.

--- test/index.js
+++ test/index.js
@@ -10,13 +10,13 @@ import crypt from '../src/index'
 describe('apache-crypt', function () {
     // Test for valid password.
     it('valid password', function () {
-        let encrypted = crypt("validPass", "B5xBYM2HbnPqI");
+        const encrypted = crypt("validPass", "B5xBYM2HbnPqI");
         expect(encrypted).to.equal("B5xBYM2HbnPqI");
     });
 
     // Test for invalid password.
     it('invalid password', function () {
-        let encrypted = crypt("invalidPass", "B5xBYM2HbnPqI");
+        const encrypted = crypt("invalidPass", "B5xBYM2HbnPqI");
         expect(encrypted).to.not.equal("B5xBYM2HbnPqI");
     });
 });
