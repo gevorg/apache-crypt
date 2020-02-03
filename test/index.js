815209d10e3e68e03757490451af4da376cc63a3
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2020-02-04 01:02:49 +0400

    Prettier added.

--- test/index.js
+++ test/index.js
@@ -1,22 +1,22 @@
 "use strict";
 
 // Expect module.
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
 // Import source.
-const crypt = require('../src/index');
+const crypt = require("../src/index");
 
 // apache-md5 tests.
-describe('apache-crypt', () => {
-    // Test for valid password.
-    it('valid password', () => {
-        const encrypted = crypt("validPass", "B5xBYM2HbnPqI");
-        expect(encrypted).to.equal("B5xBYM2HbnPqI");
-    });
+describe("apache-crypt", () => {
+  // Test for valid password.
+  it("valid password", () => {
+    const encrypted = crypt("validPass", "B5xBYM2HbnPqI");
+    expect(encrypted).to.equal("B5xBYM2HbnPqI");
+  });
 
-    // Test for invalid password.
-    it('invalid password', () => {
-        const encrypted = crypt("invalidPass", "B5xBYM2HbnPqI");
-        expect(encrypted).to.not.equal("B5xBYM2HbnPqI");
-    });
+  // Test for invalid password.
+  it("invalid password", () => {
+    const encrypted = crypt("invalidPass", "B5xBYM2HbnPqI");
+    expect(encrypted).to.not.equal("B5xBYM2HbnPqI");
+  });
 });
